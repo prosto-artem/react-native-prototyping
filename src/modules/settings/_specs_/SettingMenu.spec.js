43dd2b7f4f44cf2bdf647d6b78578be638f073ff
BrianJVarley <brianvarley9@gmail.com>
2018-04-23 21:56:47 +0100

    udpated image assets, unit test specs

--- src/modules/settings/_specs_/SettingMenu.spec.js
+++ src/modules/settings/_specs_/SettingMenu.spec.js
@@ -5,22 +5,57 @@ import {shallow} from 'enzyme';
 import {hasStyles} from '../../../../test/assertions';
 import SettingMenu from '../SettingMenu';
 
+import {
+  Text
+} from 'react-native';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger
+} from 'react-native-popup-menu';
+
+import {render, normalizeStyle, nthChild} from './helpers';
+import {createSpy, objectContaining} from 'jasmine';
+
+
+
+
 describe('<SettingMenu />', () => {
+
+  const makeMockContext = ({optionsCustomStyles, onSelect, closeMenu} = {}) => ({
+    menuActions: {
+      _getOpenedMenu: () => ({
+        optionsCustomStyles: optionsCustomStyles || {},
+        instance: {props: {onSelect: onSelect}}
+      }),
+      closeMenu: closeMenu || createSpy()
+    }
+  });
 
   it('should render a <Menu> with a menu title', () => {
     const wrapper = shallow(
       <SettingMenu/>
     );
 
-    expect(wrapper.state().menuTitle).to.equal('Measurement Units');        
+    expect(wrapper.find(Menu).exists()).toEqual(true);
+    expect(wrapper.find(MenuTrigger).exists()).toEqual(true);
+    expect(wrapper.find(Text).render().text()).toEqual('Units of Measurement: CM');
   });
 
   // it('should update menu title when option selected', () => {
-  //   const wrapper = shallow(
-  //     <SettingMenu menuTitle='Measurement Units' />
+  //   const spy = createSpy();
+  //   const { renderer } = render(
+  //     <MenuOption onSelect={spy} value='random title value' />,
+  //     makeMockContext()
   //   );
+  //   const touchable = renderer.getRenderOutput();
+  //   touchable.props.onPress();
+  //   expect(spy).toHaveBeenCalledWith('hello');
+  //   expect(spy.calls.count()).toEqual(1);
 
-  //   // expect(wrapper.props().menuTitle).to.equal('Measurement Units');        
+  //   expect(wrapper.find(Text).render().text()).not.toEqual('Units of Measurement: CM');
+
   // });
 
 });
---
+++ src/modules/settings/_specs_/helpers.js
@@ -0,0 +1,70 @@
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+/**
+ * Renders component and returns instance object and rendered output.
+ */
+export function render(Component, ctx) {
+  const renderer = new ShallowRenderer();
+  renderer.render(Component, ctx);
+  const instance = renderer._instance._instance;
+  const output = renderer.getRenderOutput();
+  return { output, instance, renderer };
+}
+
+/**
+ * Merge styles (possible array) into single style object.
+ */
+export function normalizeStyle(styles) {
+  if (Array.isArray(styles)) {
+    return styles.reduce((r, s) => Object.assign(r, s), {});
+  }
+  return styles;
+}
+
+/**
+Enable debug logs
+*/
+export function showDebug() {
+  jest.mock('../src/logger', ()=> ({
+    debug : (...args) => {
+      console.log('test-debug', ...args);
+    },
+  }));
+}
+
+/**
+Creates a mock of react instance
+*/
+export function mockReactInstance() {
+  const instance = {
+    state: {}
+  };
+  instance.setState = (newState, after) => {
+    Object.assign(instance.state, newState);
+    after && after();
+  }
+  return instance;
+}
+
+const WAIT_STEP = 50;
+export function waitFor(condition, timeout = 200) {
+  const startTime = new Date().getTime();
+  return new Promise((resolve, reject) => {
+    const check = () => {
+      const t = new Date().getTime();
+      console.log('Checking condition at time:', t - startTime);
+      if (condition()) {
+        return resolve();
+      }
+      if (t > startTime + timeout) {
+        return reject();
+      }
+      setTimeout(check, Math.min(WAIT_STEP, startTime + timeout - t));
+    };
+    check();
+  });
+}
+
+export function nthChild(node, n) {
+  return n === 0 ? node : nthChild(node.props.children, n - 1);
+}
